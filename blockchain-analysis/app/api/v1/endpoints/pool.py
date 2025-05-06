c5462db15089ed19e60a2dd425dfa790617459ad
Mathieu Dolmen <mathieu.dolmen@gmail.com>
2025-05-06 15:07:07 +0200

    [BLOCK] Get chain data in separate function

--- blockchain-analysis/app/api/v1/endpoints/pool.py
+++ blockchain-analysis/app/api/v1/endpoints/pool.py
@@ -11,21 +11,27 @@ class PoolParams(BaseModel):
     chain: str
     address: str
 
+def get_chain_data(chain, address):
+    if chain not in SUPPORTED_CHAINS:
+        return {"error": f"Unsupported chain provided: {chain}"}
+
+    # Get data from Dexscreener's API
+    url = f"https://api.dexscreener.com/token-pairs/v1/{chain}/{address}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        return {"error": "Failed to fetch data from DexScreener"}
+
+    return response.json()
+
 @router.get("/")
 async def index():
     return {"message": "ping ok"}
 
 @router.post("/largest-pool")
 async def get_largest_pool(params: PoolParams):
-    if params.chain not in SUPPORTED_CHAINS:
-        return {"error": f"Unsupported chain provided: {params.chain}"}
-
-    # Get data from Dexscreener's API
-    url = f"https://api.dexscreener.com/token-pairs/v1/{params.chain}/{params.address}"
-    response = requests.get(url)
-    if response.status_code != 200:
-        return {"error": "Failed to fetch data from DexScreener"}
-    pairs = response.json()
+    pairs = get_chain_data(params.chain, params.address)
+    if "error" in pairs:
+        return pairs
 
     # Find largest pool
     largest_pool = max(pairs, key=lambda pool: pool.get("liquidity", {}).get("usd", 0))
